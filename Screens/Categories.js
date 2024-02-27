33683022ff281e972613759abfce060d039bdc84
sbatchu35 <sbatchu35@gatech.edu>
2024-02-27 01:33:24 -0500

    categories page ui

--- Screens/Categories.js
+++ Screens/Categories.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, FlatList, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { View, FlatList, Text, StyleSheet, TouchableOpacity, Image, ScrollView } from 'react-native';
 import { getProcessedMenuData } from './menuDataAsyncStorage';
 import manipulateData from '../DataManipulation';
 
@@ -23,17 +23,30 @@ Categories = ({ navigation }) => {
   }, []);
   return (
     <View style={styles.container}>
-      <OutputData manipulatedData={manipulatedData} />
+      <Text style={styles.heading}>Your Menu</Text>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <OutputData manipulatedData={manipulatedData} />
+      </ScrollView>
     </View>
   );
 };
 
 const OutputData = ({ manipulatedData }) => {
-  console.log("manipulatedData", manipulatedData);
-
   return (
     <View style={styles.container}>
-      <Text>{JSON.stringify(manipulatedData)}</Text>
+      {manipulatedData && manipulatedData.map((foodItem, index) => (
+        <View key={index} style={styles.item}>
+          <Text style={styles.itemText}>{`${index + 1}`}. {foodItem.name}</Text>
+          <View style={styles.nutritionalValues}>
+            <Text style={styles.nutritionalValue}>{`Protein: ${foodItem.protein}`}</Text>
+            <Text style={styles.nutritionalValue}>{`Carbs: ${foodItem.carbs}`}</Text>
+            <Text style={styles.nutritionalValue}>{`Fat: ${foodItem.fat}`}</Text>
+            <Text style={styles.nutritionalValue}>{`Sodium: ${foodItem.sodium}`}</Text>
+            <Text style={styles.nutritionalValue}>{`Cholesterol: ${foodItem.cholesterol}`}</Text>
+            <Text style={styles.nutritionalValue}>{`Sugar: ${foodItem.sugar}`}</Text>
+          </View>
+        </View>
+      ))}
     </View>
   );
 };
@@ -42,33 +55,40 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#F2EFE9',
     alignItems: 'center',
-    marginTop: 60,
+    marginTop: 20,
+    flex: 1,
   },
-  preview: {
-    width: 100,
-    height: 100,
-    marginLeft: 5,
-    marginRight: 5,
-    marginBottom: 10,
-    backgroundColor: '#F2EFE9',
-    borderRadius: 10,
-    shadowColor: 'black',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.5,
-    shadowRadius: 5,
+  scrollContainer: {
+    alignItems: 'center',
+  },
+  heading: {
+    fontSize: 35,
+    marginTop: 50
   },
   item: {
-    width: 180,
-    height: 180,
+    width: 350,
+    height: 100,
     margin: 10,
     backgroundColor: '#568259',
     borderRadius: 8,
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingLeft: 10,
+    paddingTop: 10,
   },
   itemText: {
     fontSize: 20,
     color: '#F2EFE9',
   },
+  nutritionalValues: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    marginTop: 10
+  },
+  nutritionalValue: {
+    marginRight: 10,
+    fontSize: 16,
+    color: '#F2EFE9',
+  },
 });
+
 export default Categories;
